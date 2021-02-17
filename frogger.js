29def6d9b765eb695311e2ecdaf0a3dfeba02e53
timotheemcdonald <timotheemcdonald@gmail.com>
2021-02-17 17:10:11 -0500

    added graphics and script files

---
+++ frogger.js
@@ -0,0 +1,19 @@
+class Frogger {
+    constructor(){
+        this.spriteWidth = 250;
+        this.spriteHeight = 250;
+        this.width = this.spriteWidth/5;
+        this.height = this.spriteHeight/5;
+        this.x = canvas.width/2 - this.width/2;
+        this.y = canvas.height - this.height - 40;
+        this.moving = false;
+        this.frameX = 0;
+        this.frameY = 0;
+    }
+    update(){
+
+    }
+    draw(){
+        
+    }
+}
---
+++ script.js
@@ -0,0 +1,38 @@
+const canvas1 = document.getElementById('canvas1');
+const ctx1 = canvas.getContexT('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas2 = document.getElementById('canvas2');
+const ctx2 = canvas.getContexT('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas3 = document.getElementById('canvas3');
+const ctx3 = canvas.getContexT('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas4 = document.getElementById('canvas4');
+const ctx4 = canvas.getContexT('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas5 = document.getElementById('canvas5');
+const ctx5 = canvas.getContexT('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+//global variables
+const grid = 80;
+let keys = [];
+let score = 0;
+let collisionsCount = 0;
+let frame = 0;
+let gameSpeed = 1;
+
+const particlesArray = [];
+const maxParticles = 300;
+const ripplesArray = [];
+const carsArray = [];
+const logsArray = [];
